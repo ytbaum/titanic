c382c8ea9ad6a89d5e1f21242e092971326e119a
Yoni Teitelbaum <ytbaum@gmail.com>
2017-04-24 18:53:08 +0300

    initial commit: model that predicts randomly, with probability 0.5, whether each passenger survived

---
+++ model.R
@@ -0,0 +1,11 @@
+# read in the training data, testing data, and submission files
+train <- read.csv("train.csv")
+test <- read.csv("test.csv")
+submission.table <- read.csv("gender_submission.csv")
+
+# randomly generate a prediction for each passenger we have to predict for
+num.predictions <- nrow(submission.table)
+predictions <- rbinom(num.predictions, 1, 0.5)
+submission.table$Survived <- predictions
+
+write.csv(submission.table, "submission.csv")
