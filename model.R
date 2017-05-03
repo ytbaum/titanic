62208a9938bddf06c02d087bce544d6680e09f7e
Jorge Menezes <jorgefernandosaraiva@gmail.com>
2017-05-03 17:28:25 +0300

    catching up with Yoni repo

--- model.R
+++ model.R
@@ -6,7 +6,11 @@ submission.table <- read.csv("gender_submission.csv")
 ##########
 # randomly generate a prediction for each passenger we have to predict for
 num.predictions <- nrow(submission.table)
+<<<<<<< HEAD
 predictions <- rbinom(num.predictions, 1, 0.8)
+=======
+predictions <- rbinom(num.predictions, 1, 0.2)
+>>>>>>> c32d675fd036123d67c66f08e42af3fe1cdc2df2
 submission.table$Survived <- predictions
 
 write.csv(submission.table, "submission.csv", row.names = FALSE, quote = FALSE)
